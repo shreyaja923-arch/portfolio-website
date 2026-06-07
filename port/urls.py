0c4df330c8a4a406adcfb464ebf21dfe42623642
Shreya <shreyaja923@gmail.com>
2026-06-07 21:53:03 +0530

    Completed portfolio website pages and styling

--- port/urls.py
+++ port/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.home, name='about'),
-    path('contact/', views.home, name='contact'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
