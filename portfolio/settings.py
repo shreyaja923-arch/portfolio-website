d5a37836f8aa00e2f6b223b766481e6bdfe4462f
Shreya <shreyaja923@gmail.com>
2026-07-06 13:45:43 +0530

    Configure WhiteNoise routing and disable DEBUG for Vercel static serving

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1s&^c4=5n#_5hd4r9(4a_a8riwe)xxtdj4h%%x4wmslqw1)^_@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
